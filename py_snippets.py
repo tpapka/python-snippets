c179febb1935a0cf6475e60c3fc12864048ca1e7
tpapka <36523428+tpapka@users.noreply.github.com>
2019-08-21 09:43:59 -0600

    Update py_snippets.py

--- py_snippets.py
+++ py_snippets.py
@@ -3,3 +3,11 @@
 import os
 for k, v in os.environ.items():
     print("%s = %s" %(k,v))
+
+
+## Problems with speed using Internet Explorer    
+from selenium.webdriver import IeOptions
+ieo = IeOptions()
+ieo.require_window_focus = True
+driver = webdriver.Ie("G:\z-BPA\IEDriver\IEDriverServer.exe", options=ieo)
+
